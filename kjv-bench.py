42f886b3c4995748f7799c59ce65b64bad7384d3
jimbelton <jim.belton@gmail.com>
2015-11-20 14:41:07 -0800

    Adding kjv-bench.py benchmark

---
+++ kjv-bench.py
@@ -0,0 +1,35 @@
+import sys, os, subprocess, signal
+
+best_out_of = 2
+programs    = os.listdir("build")
+
+for program in programs:
+    fastest_attempt = None
+
+    for attempt in range(best_out_of):
+        proc = subprocess.Popen(['./build/'+program, "-k"], stdout=subprocess.PIPE)
+
+        # wait for the program to fill up memory and spit out its "ready" message
+        try:
+            runtime = float(proc.stdout.readline().strip())
+        except:
+            runtime = 0
+
+        ps_proc = subprocess.Popen(['ps up %d | tail -n1' % proc.pid], shell=True, stdout=subprocess.PIPE)
+        nbytes = int(ps_proc.stdout.read().split()[4]) * 1024
+        ps_proc.wait()
+
+        os.kill(proc.pid, signal.SIGKILL)
+        proc.wait()
+
+        if runtime: # otherwise it crashed
+            if attempt == 0:
+                fastest_attempt = runtime
+
+            elif attempt and runtime < fastest_attempt:
+                fastest_attempt = runtime
+
+    if not fastest_attempt:
+        print "No run of " + program + " succeeded"
+    else:
+        print "%s: %f" % (program, fastest_attempt)
--- src/template.c
+++ src/template.c
@@ -3,9 +3,48 @@
 #include <time.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <math.h>
+#include <fcntl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <unistd.h>
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
+    #include <fstream>
+//    #define O_RDONLY 0    // Hack because C++ doesn't seem to like <fcntl.h>
+
+    size_t filesize(const char* filename)
+    {
+        std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
+        return (size_t)in.tellg();
+    }
+#else
+    #include <fcntl.h>
+
+    size_t filesize(const char* filename)
+    {
+        struct stat fileStat;
+
+        if (stat(filename, &fileStat) < 0) {
+            fprintf(stderr, "Failed to stat %s\n", filename);
+            exit(1);
+        }
+
+        return fileStat.st_size;
+    }
+#endif
+
+static void usage(const char * program)
+{
+    fprintf(stderr, "usage: %s <numkeys> (sequential|random|delete|sequentialstring|randomstring|deletestring|kjvmark)\n", program);
+    exit(1);
+}
 
 double get_time(void)
 {
@@ -27,8 +66,8 @@ int main(int argc, char ** argv)
     int num_keys = atoi(argv[1]);
     int i, value = 0;
 
-    if(argc <= 2)
-        return 1;
+    if (argc <= 2)
+        usage(argv[0]);
 
     SETUP
 
@@ -77,6 +116,60 @@ int main(int argc, char ** argv)
         for(i = 0; i < num_keys; i++)
             DELETE_STR_FROM_HASH(new_string_from_integer(i));
     }
+
+    /* This benchmark counts the number of distinct words (strings on nonspace characters) in the king james bible.
+     */
+    else if (strcmp(argv[2], "kjvmark") == 0)
+    {
+        const char * file = "data/king-james-bible.txt";
+
+        if (argc >= 5 && strcmp(argv[3], "-f") == 0)
+            file = argv[4];
+
+        size_t size = filesize(file);
+        char * text = (char *)malloc(size + 1);    // + 1 allows '\0' termination of last word
+        int    fd;
+
+        if ((fd = open(file, O_RDONLY)) < 0) {
+            fprintf(stderr, "%s: Failed to open %s\n", argv[0], file);
+            exit(1);
+        }
+
+        if (read(fd, text, size) != size) {
+            fprintf(stderr, "%s: Failed to read %lu bytes from %s\n", argv[0], size, file);
+            exit(1);
+        }
+
+        before = get_time();    // Reset start time now that preparations are done
+
+        size_t start = 0;
+
+        for (;;) {
+            while (start < size && isspace(text[start]))
+                start++;
+
+            if (start == size)
+                break;
+
+            size_t endWord = start + 1;
+
+            while (endWord < size && !isspace(text[endWord]))
+                endWord++;
+
+            text[endWord] = '\0';    // '\0' terminate the "word" (actually a space delimitted sequence)
+            INSERT_STR_INTO_HASH(&text[start], value);
+            start = endWord + 1;
+        }
+
+        if (STR_HASH_GET_SIZE() != 34027) {
+            fprintf(stderr, "%s: Incorrect number of pseudo words were found in the KJV text (expected 34027, got %lu)\n", argv[0],
+                    STR_HASH_GET_SIZE());
+            exit(1);
+        }
+    }
+
+    else
+        usage(argv[0]);
 
     double after = get_time();
     printf("%f\n", after-before);
